d804b16416f73156c4d7a016b4a58fa06c3748a6
Brian Richter <brianorwhatever@gmail.com>
2023-10-06 12:15:12 -0700

    Update

--- src/Run.ts
+++ src/Run.ts
@@ -16,6 +16,8 @@ import {
   scanDir,
 } from "./utils.js";
 import path from "path";
+import chalk from "chalk";
+import figlet from "figlet";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -69,107 +71,48 @@ program
 program.parse();
 
 const main = async (): Promise<void> => {
-  // YAML Schema Validation
-  const schema = z.object({
-    config: z.string().optional(),
-    wikiPath: z
-      .string({ invalid_type_error: "wikiPath must be a string" })
-      .optional(),
-    githubRepo: z.string({ invalid_type_error: "githubRepo must be a string" }),
-    term: z
-      .string({
-        required_error: "term is required",
-        invalid_type_error: "Name must be a string",
-      })
-      .regex(/[a-z0-9_-]+/)
-      .min(1, { message: "term must be at least 1 character" }),
-    termType: z
-      .string()
-      .regex(/[a-z0-9_-]+/)
-      .optional(),
-    isa: z.string().optional(), // TODO concept of which this is a specialization.
-    glossaryText: z
-      .string({
-        required_error: "glossaryText is required",
-        invalid_type_error: "glossaryText must be a string",
-      })
-      .min(1, { message: "glossaryText must be at least 1 character" }),
-    synonymOf: z.string().optional(), // TODO check the actual type string is a placeholder
-    groupTags: z
-      .string()
-      .regex(/(?:\[\s*([a-z0-9_-]+)\s*(?:,\s*([a-z0-9_-]+))*\s*\])?/)
-      .optional(),
-    formPhrases: z.string().optional(), // TODO check the actual type string is a placeholder
-    status: z.string().optional(),
-    created: z.date().optional(),
-    updated: z.date().optional(),
-    // updated: z.string().datetime().optional(),
-    contributors: z.string().optional(),
-    attribution: z.string().optional(),
-    originalLicense: z.string().optional(),
-  });
+  var options = program.opts();
+  if (program.args[0]) {
+    options.input = program.args[0];
+  }
 
-  type ISchema = z.infer<typeof schema>;
+  console.log(
+    chalk.red(figlet.textSync("TEv2 Ingress", { horizontalLayout: "full" }))
+  );
 
-  // Command Line Arguments
-  const claOptions = [
-    { name: "config", alias: "c", type: String },
-    { name: "wikiPath", alias: "w", type: String },
-    { name: "githubRepo", alias: "r", type: String },
-    { name: "term", alias: "t", type: String },
-    { name: "termType", alias: "e", type: String },
-    { name: "isa", alias: "i", type: String },
-    { name: "glossaryText", alias: "g", type: String },
-    { name: "synonymOf", alias: "y", type: String },
-    { name: "groupTags", alias: "x", type: String },
-    { name: "formPhrases", alias: "f", type: String },
-    { name: "status", alias: "s", type: String },
-    { name: "created", alias: "d", type: Date },
-    { name: "updated", alias: "u", type: Date },
-    { name: "contributors", alias: "n", type: String },
-    { name: "attribution", alias: "a", type: String },
-    { name: "originalLicense", alias: "l", type: String },
-  ];
+  if (options.config) {
+    try {
+      const config = yaml.load(
+        fs.readFileSync(path.resolve(options.config), "utf8")
+      ) as yaml.Schema;
 
-  // Parse the command line arguments
-  const cla: ISchema = commandLineArgs(claOptions) as ISchema;
-
-  // Read in the config.yaml file
-  console.log(`Reading config file: ${cla.config ?? "config.yaml"}`);
-  const fileContents = fs.existsSync(cla.config ?? "config.yaml")
-    ? fs.readFileSync(cla.config ?? "config.yaml", "utf8")
-    : "";
-  const yml = yaml.load(fileContents) as ISchema;
-
-  // Remove any null values from the yml object
-  removeNullValues(yml);
-
-  // merge the YAML config with the command line arguments into a new single settings object. Command line arguments override the config.yaml file.
-  const settings = { ...yml, ...cla };
-
-  // Validate the object against the schema
-  const result = schema.safeParse(settings);
-
-  // If the settings object is not valid based on the schema, throw an error
-  if (!result.success)
-    throw new Error(`Config is invalid ${JSON.stringify(result, null, 2)}`);
+      // Merge config options with command line options
+      options = { ...config, ...options };
+    } catch (err) {
+      console.error(
+        `E011 Failed to read or parse the config file '${options.config}':`,
+        err
+      );
+      process.exit(1);
+    }
+  }
 
   // Validate the settings object
-  if (!settings.githubRepo)
+  if (!options.githubRepo)
     throw new Error(
       "Github Repo is required. Please provide a URL to a repo using with the -r or --githubRepo flag or githubRepo in the config.yaml file."
     );
-  console.log(`Processing Repo wiki: ${settings.githubRepo}`);
+  console.log(`Processing Repo wiki: ${options.githubRepo}`);
 
-  console.log(JSON.stringify(settings, null, 2)); // TODO might need to remove this. If there are any secrets it would leak them.
+  console.log(JSON.stringify(options, null, 2)); // TODO might need to remove this. If there are any secrets it would leak them.
 
   // Delete the cloned wiki repo directory if it alredy exists
-  fs.rmSync(settings.wikiPath ?? "./wiki", { recursive: true, force: true });
+  fs.rmSync(options.wikiPath ?? "./wiki", { recursive: true, force: true });
 
   // Clone the Github wiki repo
   await simpleGit().clone(
-    settings.githubRepo,
-    settings.wikiPath ?? "./wiki",
+    options.githubRepo,
+    options.wikiPath ?? "./wiki",
     undefined,
     (err) => {
       if (err) {
@@ -180,7 +123,7 @@ const main = async (): Promise<void> => {
   );
 
   // Scan the cloned wiki repo for markdown files
-  const files = scanDir(settings.wikiPath ?? "./wiki");
+  const files = scanDir(options.wikiPath ?? "./wiki");
   if (!fs.existsSync("curated-texts")) {
     fs.mkdirSync("curated-texts", { recursive: true });
   }
@@ -197,7 +140,7 @@ const main = async (): Promise<void> => {
   });
 
   // Delete the cloned wiki repo directory
-  fs.rmSync(settings.wikiPath ?? "./wiki", { recursive: true, force: true });
+  fs.rmSync(options.wikiPath ?? "./wiki", { recursive: true, force: true });
 };
 
 main();
